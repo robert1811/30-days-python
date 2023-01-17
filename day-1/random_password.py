60e98f035d14de96e9875f9a211c5874608fc035
Robert <robertoarvelo3@gmail.com>
2023-01-17 20:46:43 -0300

    added passowrd generator

---
+++ day-1/random_password.py
@@ -0,0 +1,18 @@
+import random
+
+def random_password():
+    letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+    password_length = None
+    password_length = input('type the size of the password: ')
+
+    array = []
+
+    if password_length.isnumeric():
+        for i in range(0, int(password_length)):
+            array.append(letters[random.randrange(0, len(letters))])
+        print(''.join(array))
+        random_password()
+    else:
+        random_password()
+random_password()
