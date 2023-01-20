07c18a73180e6bdc12f6792fa86a4bc7a988d13d
Robert <robertoarvelo3@gmail.com>
2023-01-20 17:47:06 -0300

    more OOP

--- day-4/classes.py
+++ day-4/classes.py
@@ -1,4 +1,6 @@
 class Car:
+    wheel = 4
+
     def __init__(self,make,model,year,color):
         self.make = make
         self.model = model
@@ -14,3 +16,18 @@ class Car:
 car_1 = Car('Chevy', 'Corvette', 2021, 'blue')
 
 print(car_1.drive())
+
+class Animal:
+    alive = True
+
+    def eat():
+        return 'animal is eating'
+
+class Rabbit(Animal):
+    #classes can inherit more than one class
+    
+    #methods can be overriden
+    def eat():
+        return 'rabbit is eating'
+
+print(Rabbit.eat())
