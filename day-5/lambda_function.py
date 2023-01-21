f1e84b90d0a16a150257c31737100a0b54751908
Robert <robertoarvelo3@gmail.com>
2023-01-21 14:17:50 -0300

    sort

--- day-5/lambda_function.py
+++ day-5/lambda_function.py
@@ -1,5 +1,7 @@
 #lambda functions are written in one line of code
 
 double = lambda x:x*2
+multiply = lambda x, y : x * y
 
 print(double(5))
+print(multiply(5, 2))
---
+++ day-5/sort.py
@@ -0,0 +1,7 @@
+list = [5, 8, 20, 80, 15, 21]
+
+#valid parameters: key and reverse
+list.sort()
+
+for i in list:
+    print(i)
