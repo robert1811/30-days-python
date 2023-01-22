76471450c0361c48cbc98a6a64090341c6973f63
Robert <robertoarvelo3@gmail.com>
2023-01-22 18:31:08 -0300

    zip objects

---
+++ day-6/zip.py
@@ -0,0 +1,8 @@
+usernames = ['Idontknow', 'insertusername']
+passwords = ['I like trains', 'HelloWorld']
+
+zippedUsers = zip(usernames, passwords)
+users = dict(zippedUsers)
+
+print(zippedUsers)
+print(users)
